b5718313e1ab2ab7a761d96ff0440abb642599c1
DzarZaki <dzarzaki2@gmail.com>
2023-06-21 15:34:18 +0700

    Menutup file setelah selesai menulis. Selanjutnya, membuka file dalam mode membaca menggunakan nama file yang sama. Mencetak pesan setelah berhasil membuka dan membaca file

--- ioFileWithPatch/ioFileWithPatch.cpp
+++ ioFileWithPatch/ioFileWithPatch.cpp
@@ -27,4 +27,15 @@ int main() {
 		if (baris == "q") break;
 		// menulis dan memasukan nilai dari 'baris' ke dalam file
 		outfile << baris << endl; 
-	}  
+	}
+	// selesai dalam menulis sekarang tutup filenya
+	outfile.close();
+
+	// Membuka file dalam mode membaca
+	ifstream infile;
+	// menunjuk ke sebuah file
+	infile.open(NamaFile + ".txt", ios::in);
+
+	cout << endl << ">= Membuka dan membaca file " << endl;
+	// jika file ada maka
+	if (infile.is_open())
