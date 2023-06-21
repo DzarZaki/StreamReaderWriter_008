194427a230ff9059a413f63bcb77a9d30d4b5ca9
DzarZaki <dzarzaki2@gmail.com>
2023-06-21 15:42:41 +0700

    Menambahkan blok catch untuk menangkap pengecualian dengan tipe data integer. Blok ini akan dieksekusi jika pengecualian dengan tipe data integer dilemparkan. Juga menambahkan blok catch untuk menangkap pengecualian dengan tipe data selain integer. Blok ini akan dieksekusi jika pengecualian dengan tipe data selain integer dilemparkan. Mengembalikan nilai 0.

--- manualExeption/manualExeption.cpp
+++ manualExeption/manualExeption.cpp
@@ -9,4 +9,15 @@ int main()
 		throw 0.5; // melemparkan sebuh integer maka
 		cout << "Pernyataan tidak akan diexsekusi" << endl;
 	}
-	
+	catch (int a) {
+		// blok ini akan dieksekusi
+		cout << "Pengecualian akan diexsekusi" << endl;
+
+	}
+	catch (...) {
+		//*jika selain integer maka blok ini akan diexsekusi
+		cout << "default Penggecualian diexsekusi" << endl;
+	}
+
+	return 0;
+}
